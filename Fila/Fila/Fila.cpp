965af2860dfd4d46571e09b49f53a99daa2eb45d
Sérgio da Rocha <sergio.alves13@fatec.sp.gov.br>
2023-04-18 23:40:20 -0300

    Função inserir modificada para atingir os objetivos solicitados. Funções remover e exibir elementos foram implementadas.

--- Fila/Fila/Fila.cpp
+++ Fila/Fila/Fila.cpp
@@ -15,6 +15,7 @@ void menu();
 void inicializar();
 void insere();
 void remove();
+void exibirElementos();
 //--------------------------
 
 
@@ -26,14 +27,15 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 4) {
+	while (op != 5) {
 		system("cls"); // somente no windows
 		cout << "Menu Fila";
 		cout << endl << endl;
 		cout << "1 - Inicializar Fila \n";
 		cout << "2 - Inserir elemento \n";
 		cout << "3 - Remover elemento  \n";
-		cout << "4 - Sair \n";
+		cout << "4 - Exibir elemento\n";
+		cout << "5 - Sair \n";
 
 		cout << "Opcao: ";
 		cin >> op;
@@ -46,7 +48,9 @@ void menu()
 			break;
 		case 3: remove();
 			break;
-		case 4:
+		case 4: exibirElementos();
+			break;
+		case 5:
 			return;
 		default:
 			break;
@@ -88,13 +92,53 @@ void insere()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
+	if (inicio == NULL)
+	{
+		inicio = novo;
+		fim = novo;
+		return;
+	}
 
+	else
+	{
+		fim->prox = novo;
+		fim = novo;
+		return;
+	}
 }
 
 void remove()
 {
-
+	if (inicio == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	
+	else
+	{
+		NO* aux = inicio->prox;
+		cout << "O primeiro elemento, de valor " << inicio->valor << ", foi removido da lista.\n";
+		free(inicio);
+		inicio = aux;
+		return;
+	}
+	
 
 
 }
 
+void exibirElementos()
+{
+	if (inicio == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = inicio;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->prox;
+		}
+	}
+}
